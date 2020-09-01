e53fd416b8d8a1559998cc74f2506c17109795c3
GunjanKadu <gunjan.kadu@de.abb.com>
2020-09-01 15:29:57 +0200

    Styling Header component

--- google-clone/src/App.tsx
+++ google-clone/src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Google Clone</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>The is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+            {/* Home Page */}
+          </Route>
+          {/* {Search page -> Where the results are shown} */}
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ google-clone/src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="about">About</Link>
+          <Link to="about">Store</Link>
+          {/* Link */}
+        </div>
+        <div className="home__headerRight">
+          <Link to="about">Gmail</Link>
+          <Link to="about">Images</Link>
+          <AppsIcon />
+          <Avatar />
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt="google icon"
+        />
+      </div>
+    </div>
+  );
+}

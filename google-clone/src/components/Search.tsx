1192db3a9e35b7ad3302a88b8ceb789c99a179a7
GunjanKadu <gunjan.kadu@de.abb.com>
2020-09-01 16:31:09 +0200

    Finalizing search bar

--- google-clone/src/components/Search.tsx
+++ google-clone/src/components/Search.tsx
@@ -4,18 +4,24 @@ import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
 import SelectInput from "@material-ui/core/Select/SelectInput";
+import { useHistory } from "react-router-dom";
 
 type ButtonEvent = React.MouseEvent<HTMLButtonElement>;
 type FormEvent = React.ChangeEvent<HTMLInputElement>;
 
-function Search() {
-  const [input, setinput] = useState("");
+interface searchProps {
+  hideButtons?: boolean;
+}
 
+function Search(prop: searchProps) {
+  const [input, setinput] = useState("");
+  const history = useHistory();
   const search = (e: ButtonEvent) => {
     e.preventDefault();
+    history.push("/search");
   };
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
         <input
@@ -24,13 +30,29 @@ function Search() {
         />
         <MicIcon />
       </div>
-      <div className="search__buttons">
-        <Button onClick={search} variant="outlined">
-          Google Search
-        </Button>
-        <Button variant="outlined">I'm Feeling Lucky</Button>
-      </div>
-    </div>
+      {!prop.hideButtons ? (
+        <div className="search__buttons">
+          <Button type="submit" onClick={search} variant="outlined">
+            Google Search
+          </Button>
+          <Button variant="outlined">I'm Feeling Lucky</Button>
+        </div>
+      ) : (
+        <div className="search__buttons">
+          <Button
+            className="search__buttonsHidden"
+            type="submit"
+            onClick={search}
+            variant="outlined"
+          >
+            Google Search
+          </Button>
+          <Button variant="outlined" className="search__buttonsHidden">
+            I'm Feeling Lucky
+          </Button>
+        </div>
+      )}
+    </form>
   );
 }
 
